4f3f80a0f6ba84c66a18ecb91a0ba755aea73171
Soroka Anatolii <sorokaanatolii@gmail.com>
2015-09-21 12:59:07 +0300

    added check_ui option

--- fixture/db.py
+++ fixture/db.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import mysql.connector
 from model.group import Group
 
--- test/test_del_group.py
+++ test/test_del_group.py
@@ -3,7 +3,7 @@ from model.group import Group
 import random
 
 
-def test_delete_some_group(gen, db):
+def test_delete_some_group(gen, db, check_ui):
     if len(db.get_group_list()) == 0:
         gen.group.create(Group(name="test"))
     old_groups = db.get_group_list()
@@ -13,3 +13,5 @@ def test_delete_some_group(gen, db):
     assert len(old_groups) - 1 == gen.group.count()
     old_groups.remove(group)
     assert old_groups == new_groups
+    if check_ui:
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(gen.group.get_group_list(), key=Group.id_or_max)
